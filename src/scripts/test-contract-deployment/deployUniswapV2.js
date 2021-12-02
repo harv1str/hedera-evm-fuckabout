1d419fe70160b21bab75af7cfbdd20341fa1d2a4
harv1str <harv1str@protonmail.com>
2021-12-03 01:43:02 +0900

    Added initial failed Uniswap V2 deployment

---
+++ src/scripts/test-contract-deployment/deployUniswapV2.js
@@ -0,0 +1,68 @@
+require("dotenv").config();
+
+const {
+  Client,
+  AccountId,
+  PrivateKey,
+  FileCreateTransaction,
+  ContractCreateTransaction
+} = require("@hashgraph/sdk");
+
+const uniswapObject = require("../contracts/uniswap_v2_pair.json");
+
+async function main() {
+    let client;
+
+    try {
+        client = Client.forName(process.env.HEDERA_NETWORK).setOperator(
+            AccountId.fromString(process.env.OPERATOR_ID),
+            PrivateKey.fromString(process.env.OPERATOR_KEY)
+        )
+    } catch (err) {
+        console.log(err);
+    }
+
+
+    const uniswapFactoryByteCode = uniswapObject.object;
+
+
+    // First, we create a file Transaction to reserve space and a contract storage on the Hashgraph
+    // This is an essential Step because it actually puts the bytecode onto an accessible state.
+    const fileTransactionResponse = await new FileCreateTransaction()
+        .setKeys([client.operatorPublicKey])
+        .setContents(uniswapFactoryByteCode)
+        .execute(client);
+
+    // There are other helper functions
+    const fileReceipt = await fileTransactionResponse.getReceipt(client);
+    
+    // file ID on Hashgraph
+    const fileId = fileReceipt.fileId;
+
+    /* Now.  We create the fucking Contract, yo */
+
+    const contractTransactionResponse = await new ContractCreateTransaction()
+        .setGas(75000)
+        .setBytecodeFileId(fileId)
+        .setAdminKey(client.operatorPublicKey)
+        .execute(client);
+    
+    const contractReceipt = await contractTransactionResponse.getReceipt(client);
+
+    const contractId = contractReceipt.contractId;
+    
+    const contract_info = {
+        network: process.env.HEDERA_NETWORK,
+        contract_id: contractId,
+        contract_solidity_address: contractId.fromSolidityAddress(),
+        file_id: fileId
+    }
+
+    console.log("Contract Deployed! Info:\n");
+    console.log(JSON.stringify(contract_info))
+    console.log("\n");
+
+    return 0;
+}
+
+main();
